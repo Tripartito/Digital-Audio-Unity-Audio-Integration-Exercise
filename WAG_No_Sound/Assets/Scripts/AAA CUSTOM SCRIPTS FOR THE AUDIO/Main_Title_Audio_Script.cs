fe6b70088821cdaea9b193c0f6e6cd653ba1d587
DidacRomero <36265669+DidacRomero@users.noreply.github.com>
2020-05-09 02:03:16 +0200

    Completed all Goals for part 1A

---
+++ WAG_No_Sound/Assets/Scripts/AAA CUSTOM SCRIPTS FOR THE AUDIO/Main_Title_Audio_Script.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main_Title_Audio_Script : MonoBehaviour
+{
+
+    public AudioSource pre_enter;
+    public AudioSource main_loop;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pre_enter.Play(0);
+
+        main_loop.loop = true;
+        main_loop.PlayDelayed(6.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
