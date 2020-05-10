78b993a63852988bd222c1d43f8809867e0e2fea
DidacRomero <36265669+DidacRomero@users.noreply.github.com>
2020-05-10 17:16:51 +0200

    Finished exercise 1 all goals.

---
+++ WAG_No_Sound/Assets/Scripts/AAA CUSTOM SCRIPTS FOR THE AUDIO/Player_Triggers.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Triggers : MonoBehaviour
+{
+    public Main_Scene_Music_Manager m_manager;
+
+    public Collider Village;
+    public Collider Cave;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Here we will call the music manager script to do stuff accordingly
+        if (other == Village || other == Cave)
+        {
+            m_manager.DetectMusicChange(other);
+        } 
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other == Village || other == Cave)
+        {
+            m_manager.ReturningToForest();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
